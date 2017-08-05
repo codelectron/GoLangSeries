92039efd8bf6812dc695ec3bf2d5469b22aee724
code <code@code.code>
2017-08-05 06:52:57 +0200

    Add function

--- Function/func_with_parameter_2.go
+++ Function/func_with_parameter_2.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 fmt.Println(addMe(2,3)) 
+fmt.Println(addMe(1,3)) 
+fmt.Println(addMe(2,4)) 
+fmt.Println(addMe(3,3)) 
 }
 
 func addMe(first int, second int   ) ( string, int) { 
